4c3a06cc5ef05c756d8109c4856a3d4536b61959
sude <sudefidan@icloud.com>
2024-10-17 21:20:39 +0300

    mvc-giris

---
+++ 03122023_MVCGiris/03122023_MVCGiris/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _03122023_MVCGiris.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Senin uygulamanın açıklaması";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Sayfa4()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ 03122023_MVCGiris/03122023_MVCGiris/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_03122023_MVCGiris.MvcApplication" Language="C#" %>
